01314ed4298e762f0f24417b7252f9ff7c9e6efb
Mellis <desibelgenet@yahoo.com>
2021-01-14 17:07:39 +0100

    Created  DOM elements

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-const url='https://swapi.dev/api/planets/';
+const url='https://swapi.dev/api/people/';
 
-function ftechData(){
-   let arr= [];
-   
-    fetch(url).then(function(res){
-        return res.json();
-    }).then(function(data){
-        data.results.map(arr1=>{
-            arr.push(arr1)
-        })
-        
+  function fetchData(){
+       fetch(url).then(  function(res){
+         return res.json();
+     }).then(function(data){ 
+         const peoples= data.results.map(function(item){
+             console.log(item.name);
+             return item.name;
+         })   
+         peoples.forEach(element => {
+             let li = document.createElement('li');
+             li.appendChild(document.createTextNode(element));
+
+                li.style.fontSize='24px';
+                li.style.fontFamily= 'Roboto';
+                li.style.fontStyle= 'normal';
+                li.style.fontWeight= 'normal';
+                li.style.fontSize= '24px';
+                li.style.lineHeight='28px';
+                li.style.letterSpacing='0.15em';
+                li.style.padding='1';
+                
+             document.querySelector('ul').appendChild(li);
+         });     
     })
-   
-    return arr
-}
-console.log(ftechData());
+ }
+
+ fetchData()
+ 
+
+ 
+    
+
+
 
